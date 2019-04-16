a5df47340f964f3a3f0016eb4518fe495b019a11
Jake <jtbartlow@gmail.com>
2019-04-16 10:48:11 -0500

    created_migration_and_classes

---
+++ db/migrate/20190416153027_create_wallets.rb
@@ -0,0 +1,9 @@
+class CreateWallets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :wallets do |t|
+      t.integer :user_id
+      t.ingeger :coin_id
+      t.float :quantity 
+  end
+end
+end
