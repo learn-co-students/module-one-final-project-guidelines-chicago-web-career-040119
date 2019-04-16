220e085304f9fa4f7c3698a83dc9e67547657998
Michael Wnek <beatawnek@Beatas-MacBook-Pro.local>
2019-04-16 11:27:28 -0500

    saving progress

--- db/migrate/20190416155318_create_horoscopes.rb
+++ db/migrate/20190416155318_create_horoscopes.rb
@@ -1,4 +1,13 @@
 class CreateHoroscopes < ActiveRecord::Migration[5.2]
   def change
+    create_table :horoscopes do |t|
+      t.datetime :date_range
+      t.datetime :current_date
+      t.text :description
+      t.string :compatibility
+      t.string :mood
+      t.string :color
+      t.integer :lucky_number
+      t.datetime :lucky_time
   end
 end
