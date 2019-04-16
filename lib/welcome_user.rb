c771c3d7621c806a03961e26c2b99c0cd6384ed7
Jackson Robbins <jackrobbins1@gmail.com>
2019-04-16 14:59:06 -0500

    pulled new michael branch

---
+++ lib/welcome_user.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+def slow_print(text)
+  (0..text.length).each { |num|
+    print text[num]
+    sleep(0.03)
+  }
+end
+
+def output_spacer
+  puts "\n"
+  2.times {|i| puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~:>"}
+end
+
+def welcome_message
+  message = "Welcome to the Virtual Horoscope Reader"
+  slow_print(message)
+  output_spacer
+end
+
+def commands_message
+  message = "Enter 'commands' to see all available commands"
+  slow_print(message)
+  output_spacer
+end
+
+def method_name
+  message = "Enter your name to log in and get horoscope"
+  slow_print(message)
+  output_spacer
+end
+
+
+
+binding.pry
+"wut"
