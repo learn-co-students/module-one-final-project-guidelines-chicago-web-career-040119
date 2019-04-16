7cefe03b3fa1048c8e7492b34861e4d014e067eb
Michael Wnek <beatawnek@Beatas-MacBook-Pro.local>
2019-04-16 14:53:24 -0500

    User class change

--- app/model/user.rb
+++ app/model/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has many :readings
   has many :horoscopes, through: :readings
+
+  
+
 end
--- bin/run.rb
+++ bin/run.rb
@@ -2,12 +2,39 @@ require_relative '../config/environment'
 require 'rest-client'
 require 'json'
 require 'pry'
+require 'zodiac'
+require 'date'
+
 
 response_string = RestClient.post('https://aztro.sameerkumar.website/', :sign => 'aries', :day => 'today'
 response_hash = JSON.parse(response_string)
 
 binding.pry
 
+def get_name_from_user
+  puts "Please enter your name"
+  gets.chomp
+end
+
+def get_birthdate_from_user
+  puts "Please enter your birthdate as" + Date.new("YYYY/MM/DD").zodiac_sign
+  gets.chomp
+end
+
+def get_sign_for_user
+  zsign = Date.new().zodiac_sign
+
+  puts "Your horoscope sign is #{zsign}"
+
+end
 
 
-puts "HELLO WORLD"
+def whoami?
+  self.name
+end
+
+# def change_user
+#
+# end
+
+#puts "HELLO WORLD"
